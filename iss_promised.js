a201ce16944af76cb04c483a508175f2c87ec222
Jonathan Phair <jmphair@gmail.com>
2023-01-19 20:29:07 -0500

    added fetchISSFlyOverTimes and it runs

--- iss_promised.js
+++ iss_promised.js
@@ -9,7 +9,11 @@ const fetchCoordsByIP = function(body) {
   return request(`http://ipwho.is/${ip}`);
 };
 
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
 
 
-
-module.exports = { fetchMyIP, fetchCoordsByIP,  };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
