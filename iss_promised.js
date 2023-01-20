6b1daef6b5bea84e89f3e68df7b6e91f4e4a9e84
Jonathan Phair <jmphair@gmail.com>
2023-01-19 20:26:32 -0500

    added fetchCoordsByIP and it is working

--- iss_promised.js
+++ iss_promised.js
@@ -4,7 +4,12 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
 
 
 
-module.exports = { fetchMyIP };
+
+module.exports = { fetchMyIP, fetchCoordsByIP,  };
